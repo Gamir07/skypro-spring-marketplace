52963003eeefa3c41b31f8a281b244f4b8849656
Gamir07 <gamir07@yandex.ru>
2023-11-16 23:21:41 +0300

    init commit

---
+++ src/main/java/pro/sky/marketplace/skyprospringmarketplace/SkyproSpringMarketplaceApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.marketplace.skyprospringmarketplace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkyproSpringMarketplaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkyproSpringMarketplaceApplication.class, args);
+	}
+
+}
